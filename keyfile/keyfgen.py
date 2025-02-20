778b40c29dd911b869ea808d12958a8ad365dda3
Andrey Grigoryev <andrey-grigorev-2006@yandex.ru>
2025-02-20 04:31:49 +0300

    clean up keyfgen a bit

--- keyfile/keyfgen.py
+++ keyfile/keyfgen.py
@@ -1,4 +1,5 @@
-import argparse, crcmod
+import argparse
+import crcmod
 from Cryptodome.Cipher import AES
 
 jl_crc16 = crcmod.mkCrcFun(0x11021,     initCrc=0x0000,     rev=False)
@@ -6,36 +7,40 @@ jl_crc32 = crcmod.mkCrcFun(0x104C11DB7, initCrc=0x26536734, rev=True)
 
 ################################################################################
 
-def int0(s):
-    return int(s, 0)
+def hexint(s):
+    return int(s, 16)
 
 ap = argparse.ArgumentParser(description='JieLi keyfile generator')
 
 ap.add_argument('-o', '--out', metavar='FILE',
                 help='Output file (if omitted no file will be generated)')
 
-ap.add_argument('--val2', type=int0, default=0xa000,
-                help="Second hex value, normally you don't want to change this. (default: 0x%(default)04X)")
-
-ap.add_argument('key', type=int0,
-                help='Chipkey to encode')
+ap.add_argument('key', type=hexint,
+                help='Chipkey to encode (a 16-bit hexadecimal number)')
 
 args = ap.parse_args()
 
 ################################################################################
 
-kc_dat = bytes('%04x%04x32kagami' % (args.key, args.val2), 'ascii')
-kc_key = AES.get_random_bytes(8) * 2
+# generate the data payload
+data = f'{args.key:04X}A000\0'.encode()
+data += b'\xff' * (16 - len(data))
 
-kc_dat = AES.new(kc_key, AES.MODE_ECB).encrypt(kc_dat)
+# encrypt that with AES
+key = AES.get_random_bytes(8) * 2
+data = AES.new(key, AES.MODE_ECB).encrypt(data) + key
 
-keyfile = bytes(kc_dat.hex() + kc_key.hex(), 'ascii')
-keyfile += bytes('%08x' % jl_crc32(keyfile), 'ascii')
+# append a CRC32 checksum
+data = data.hex()
+data += f'{jl_crc32(data.encode()):08x}'
 
-crcstring = '%04X-%08X' % (jl_crc16(keyfile), jl_crc32(keyfile))
+# ... and that's it.
+keyfile = data.encode()
+
+crcstring = f'{jl_crc16(keyfile):04X}-{jl_crc32(keyfile):08X}'
 
 print('CRC:', crcstring)
-print('keyfile: [%s]' % str(keyfile, 'ascii'))
+print(f'keyfile: [{keyfile.decode()}]')
 
 if args.out is not None:
     with open(args.out, 'wb') as f:
